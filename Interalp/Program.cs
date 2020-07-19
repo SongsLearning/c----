bec7a4ddff9bb133d9363f4a1573806391971f72
Song <217.Songs.SH@gmail.com>
2020-07-19 12:07:32 +0900

    a

---
+++ Interalp/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+using static System.Console;
+
+namespace ConsoleApp2
+{
+
+    class sideTask
+    {
+        int count;
+
+        public sideTask(int Count)
+        {
+            this.count = Count;
+        }
+
+        public void keepAlive()
+        {
+            try
+            {
+                Thread.SpinWait(1000000);
+
+                while (count > 0)
+                {
+                    WriteLine(count-- + "left");
+                    Thread.Sleep(10);
+                }
+                WriteLine("count : 0");
+            }
+
+            catch (ThreadAbortException e)
+            {
+                WriteLine(e);
+
+            }
+            finally
+            {
+                WriteLine("리소스");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            sideTask task = new sideTask(100);
+
+            Thread t1 = new Thread(new ThreadStart(task.keepAlive));
+
+
+            t1.IsBackground = false;
+
+            t1.Start();
+
+            Thread.Sleep(100);
+
+            t1.Interrupt();
+
+            t1.Join();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Thread/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using static System.Console;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void pp()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("pp! : " + i);
+                Thread.Sleep(10);//m/s 사용하고 1/1000초 스레드 지연 점유를 내려 놓는다.
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(new ThreadStart(pp));
+
+            Console.WriteLine("시작");
+            t1.Start();
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("main! : " + i);
+                Thread.Sleep(10);
+            }
+
+
+            Console.WriteLine("정지");
+            t1.Join();
+            Console.WriteLine("끝");
+
+        }
+    }
+}
